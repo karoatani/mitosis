5eed0772cc60dd6b22fbc6fed99a5262d4391586
karoatani <karoatani@gmail.com>
2024-10-30 20:26:05 +0100

    changed preloader

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Card from "./components/Card";
 import FloatingCharacter from "./components/FloatingCharacter";
 import HalloweenEffect from "./components/HalloweenEffect";
 import { useEffect, useState} from "react";
-import { Skeleton } from "antd";
+import { RiseLoader } from "react-spinners";
 
 // import {Cube} from 'react-preloaders';
 import axios from "axios";
@@ -81,7 +81,7 @@ export default function App() {
     
   return (
     <>
-    {loading ?<Skeleton/>: <div className="bg-[#573BFF] w-full overflow-x-hidden  relative flex flex-col    gap-10 text-white  bg-no-repeat bg-cover p-4 font-montserrat">
+    {loading ?<div className="flex items-center justify-center w-screen h-screen"><RiseLoader /></div> : <div className="bg-[#573BFF] w-full overflow-x-hidden  relative flex flex-col    gap-10 text-white  bg-no-repeat bg-cover p-4 font-montserrat">
       <div>
         <h1 className="text-3xl md:text-5xl uppercase font-bold font-creepster tracking-wider ">
           Morsedn404
